37de8b4582f669faef14f0a1a4f323c520edd043
PuthearSMP <bora.chea.sompom@gmail.com>
2022-04-11 15:58:41 +0700

    apply update support

---
+++ Assets/Scripts/EditorControl/Quest/QuestConstance.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class QuestConstance
+{
+	public const float TAP_PER_SECOND = 4;
+}
--- Assets/Scripts/EditorControl/Quest/QuestGameLevelDefine/QuestGameLevelUpdateHeroLvDefine.cs
+++ Assets/Scripts/EditorControl/Quest/QuestGameLevelDefine/QuestGameLevelUpdateHeroLvDefine.cs
@@ -3,6 +3,20 @@ public class QuestGameLevelUpdateHeroLvDefine : QuestGameLevelBaseDefine
 {
 	public override int GameLevelDefine()
 	{
-		return 0;
+		var levelTarget = questData.target;
+		var gameLv = 0;
+		var dmgBalance = new SMPNum();
+		var dmgTarget = EditorController.instance.GetHeroDmg(levelTarget);
+		do
+		{
+			gameLv++;
+			//var hp = EditorController.instance.GetBossHp(gameLv);
+			var hp = EditorController.instance.GetGhostHp(gameLv);
+			var tap = EditorController.instance.TapPerSec;
+			dmgBalance = hp / (QuestConstance.TAP_PER_SECOND * tap);
+		}
+		while (dmgBalance < dmgTarget);
+
+		return gameLv;
 	}
 }
