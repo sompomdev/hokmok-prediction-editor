9de113de73582f6462f903c34edc1fe17ec28df1
PuthearSMP <bora.chea.sompom@gmail.com>
2022-04-11 17:14:05 +0700

    Update gold earning

--- Assets/Scripts/EditorControl/Quest/QuestConstance.cs
+++ Assets/Scripts/EditorControl/Quest/QuestConstance.cs
@@ -5,4 +5,5 @@ using UnityEngine;
 public static class QuestConstance
 {
 	public const float TAP_PER_SECOND = 4;
+	public const int GHOST_PER_WAVE = 9;
 }
--- Assets/Scripts/EditorControl/Quest/QuestGameLevelDefine/QuestGameLevelUpdateHeroLvDefine.cs
+++ Assets/Scripts/EditorControl/Quest/QuestGameLevelDefine/QuestGameLevelUpdateHeroLvDefine.cs
@@ -3,6 +3,27 @@ public class QuestGameLevelUpdateHeroLvDefine : QuestGameLevelBaseDefine
 {
 	public override int GameLevelDefine()
 	{
+		return DefineForGoldEarningBallance();
+	}
+
+	private int DefineForGoldEarningBallance()
+	{
+		var levelTarget = questData.target;
+		var gameLv = 0;
+		var goldEarning = new SMPNum(0);
+		var goldNeed = SMPHeroLevelConfiguration.GetCostOnLevel(5, levelTarget);
+		do
+		{
+			gameLv++;
+			goldEarning += EditorController.instance.GetGoldToDrop(gameLv) * QuestConstance.GHOST_PER_WAVE + 1;//ghost and boss gold drop
+		}
+		while (goldEarning < goldNeed);
+
+		return gameLv;
+	}
+
+	private int DefineForDMGBallance()
+	{
 		var levelTarget = questData.target;
 		var gameLv = 0;
 		var dmgBalance = new SMPNum();
@@ -10,8 +31,8 @@ public class QuestGameLevelUpdateHeroLvDefine : QuestGameLevelBaseDefine
 		do
 		{
 			gameLv++;
-			//var hp = EditorController.instance.GetBossHp(gameLv);
-			var hp = EditorController.instance.GetGhostHp(gameLv);
+			var hp = EditorController.instance.GetBossHp(gameLv);
+			//var hp = EditorController.instance.GetGhostHp(gameLv);
 			var tap = EditorController.instance.TapPerSec;
 			dmgBalance = hp / (QuestConstance.TAP_PER_SECOND * tap);
 		}
