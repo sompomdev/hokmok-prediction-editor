1c878f0b06fc564508c919344c99eb5b0838e2eb
PuthearSMP <bora.chea.sompom@gmail.com>
2022-04-13 08:52:33 +0700

    apply using mana

---
+++ Assets/Scripts/EditorControl/Quest/QuestGameLevelDefine/QuestGameLevelStartUsingManaDefine.cs
@@ -0,0 +1,18 @@
+
+public class QuestGameLevelStartUsingManaDefine : QuestGameLevelBaseDefine
+{
+	public override int GameLevelDefine()
+	{
+		var dataSkill = EditorDatas.instance.GetSkillData(0);
+		var costSkill = SMPActiveSkillLevelConfiguration.GetNextCostConfiguration(dataSkill, 1);
+		var heroLvTarget = dataSkill.Level_Unlock;
+		var gameLevelHeroCanReachLevel = GetGameLevelHeroCanReachLevel(heroLvTarget, costSkill);
+
+		var manaConsume = SMPActiveSkillLevelConfiguration.GetNextManaConsumtion(dataSkill, 1);
+		var manaFarmingPerminute = SMPManaLevelConfiguration.GetManaPerMinutes();
+		var totalTimeMana = (manaFarmingPerminute / 60);
+		var gameLevelOnFarmingMana = GetGameLevelCanReachBaseOnTime(totalTimeMana);
+
+		return gameLevelHeroCanReachLevel + gameLevelOnFarmingMana;
+	}
+}
