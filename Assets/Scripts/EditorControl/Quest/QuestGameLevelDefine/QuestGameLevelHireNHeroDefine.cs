d4cdae631a6c9e4afbab87d04f3bfad6589a3c29
PuthearSMP <bora.chea.sompom@gmail.com>
2022-04-18 15:52:33 +0700

    Apply hire hero for game level define

---
+++ Assets/Scripts/EditorControl/Quest/QuestGameLevelDefine/QuestGameLevelHireNHeroDefine.cs
@@ -0,0 +1,12 @@
+using System;
+
+public class QuestGameLevelHireNHeroDefine : QuestGameLevelBaseDefine
+{
+	public override int GameLevelDefine()
+	{
+		var heroCount = questData.target;
+		heroCount = Math.Min(heroCount, QuestConstance.MAX_HERO - 1);//-1 is one hero unlock ready
+		var gameLevelUnlockHero = (heroCount) * QuestConstance.UNLOCK_HERO_STAGE_COUNT;
+		return gameLevelUnlockHero;
+	}
+}
