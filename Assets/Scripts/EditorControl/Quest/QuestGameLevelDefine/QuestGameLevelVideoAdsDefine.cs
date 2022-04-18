9df49c0f74d0441c19e7ef3252848ca014f7aab2
PuthearSMP <bora.chea.sompom@gmail.com>
2022-04-18 15:34:04 +0700

    Apply video ads game level define

--- Assets/Scripts/EditorControl/Quest/QuestGameLevelDefine/QuestGameLevelVideoAdsDefine.cs
+++ Assets/Scripts/EditorControl/Quest/QuestGameLevelDefine/QuestGameLevelVideoAdsDefine.cs
@@ -3,6 +3,9 @@ public class QuestGameLevelVideoAdsDefine : QuestGameLevelBaseDefine
 {
 	public override int GameLevelDefine()
 	{
-		return 0;
+		var videoCount = questData.target;
+		var timePerAds = QuestConstance.TIME_PER_BIRD_APPEAR + QuestConstance.TIME_PER_BIRD_APPEAR * QuestConstance.PERCENT_VIDEO_BIRD;
+		var timeAvailableAllChest = videoCount * timePerAds;
+		return GetGameLevelCanReachBaseOnTime(timeAvailableAllChest);
 	}
 }
