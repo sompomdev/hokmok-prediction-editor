012678e5f15f80bc31a5d5ae03a984c5fed6e4b7
PuthearSMP <bora.chea.sompom@gmail.com>
2022-04-19 10:59:13 +0700

    Refactor Mana potion into perk class

--- Assets/Scripts/EditorControl/Quest/QuestGameLevelDefine/QuestGameLevelUsePerkDefine.cs
+++ Assets/Scripts/EditorControl/Quest/QuestGameLevelDefine/QuestGameLevelUsePerkDefine.cs
@@ -5,6 +5,31 @@ public class QuestGameLevelUsePerkDefine : QuestGameLevelBaseDefine
 	public override int GameLevelDefine()
 	{
 		var perkId = questData.target;
+		if (perkId == 3)//Mana potion perk
+		{
+			return GameLevelManaPotionDefine();
+		}
+
+		return GameLevelPerkDefine(perkId);
+	}
+
+	public int GameLevelManaPotionDefine()
+	{
+		var manaPerk = EditorDatas.instance.GetShopSkillData(3);
+		var diamondNeed = int.Parse(manaPerk.m_Gems_Count);
+		var lvDiamondFarm = GetGameLevelByDiamondBossDrop(diamondNeed);
+
+		var firstSkill = EditorDatas.instance.GetSkillData(0);
+		var costSkill = SMPActiveSkillLevelConfiguration.GetNextCostConfiguration(firstSkill, 1);
+		var heroLvTarget = firstSkill.Level_Unlock;
+		var gameLevelHeroCanReachLevel = GetGameLevelHeroCanReachLevel(heroLvTarget, costSkill);
+
+
+		return Math.Max(lvDiamondFarm, gameLevelHeroCanReachLevel);
+	}
+
+	public int GameLevelPerkDefine(int perkId)
+	{
 		var perkData = EditorDatas.instance.GetShopSkillData(perkId);
 		var diamondNeed = int.Parse(perkData.m_Gems_Count);
 		var lvDiamondFarm = GetGameLevelByDiamondBossDrop(diamondNeed);
--- Assets/Scripts/EditorControl/Quest/QuestGameLevelDefine/QuestGameLevelUsePerkNTimeDefine.cs
+++ Assets/Scripts/EditorControl/Quest/QuestGameLevelDefine/QuestGameLevelUsePerkNTimeDefine.cs
@@ -5,6 +5,31 @@ public class QuestGameLevelUsePerkNTimeDefine : QuestGameLevelBaseDefine
 	public override int GameLevelDefine()
 	{
 		var perkId = questData.target;
+		if(perkId == 3)//Mana potion perk
+		{
+			return GameLevelManaPotionNTimeDefine();
+		}
+
+		return GameLevelPerkNTimeDefine(perkId);
+	}
+
+	public int GameLevelManaPotionNTimeDefine()
+	{
+		var perkUse = questData.target;
+		var perkData = EditorDatas.instance.GetShopSkillData(3);
+		var diamondNeed = int.Parse(perkData.m_Gems_Count) * perkUse;
+		var lvDiamondFarm = GetGameLevelByDiamondBossDrop(diamondNeed);
+
+		//this skill have cooldown before next avaialble use
+		var timeCooldown = QuestConstance.MANA_POTION_DURATION_COOL_DOWN * (perkUse - 1);//-1 refer to the first is not cooldown
+		var lvCoolDown = GetGameLevelCanReachBaseOnTime(timeCooldown);
+		lvCoolDown += GetGameLevelByDiamondBossDrop(int.Parse(perkData.m_Gems_Count));//because the first one not include time cooldown
+
+		return Math.Max(lvDiamondFarm, lvCoolDown);
+	}
+
+	public int GameLevelPerkNTimeDefine(int perkId)
+	{
 		var perkUse = questData.target2;
 		var perkData = EditorDatas.instance.GetShopSkillData(perkId);
 		var diamondNeed = int.Parse(perkData.m_Gems_Count) * perkUse;
