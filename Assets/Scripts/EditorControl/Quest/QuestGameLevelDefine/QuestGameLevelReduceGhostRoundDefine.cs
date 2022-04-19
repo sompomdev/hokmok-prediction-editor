8460dc820b19ebf0d477c2806cac582950fb7584
PuthearSMP <bora.chea.sompom@gmail.com>
2022-04-19 15:22:43 +0700

    Apply game level define to Reduce ghost round

---
+++ Assets/Scripts/EditorControl/Quest/QuestGameLevelDefine/QuestGameLevelReduceGhostRoundDefine.cs
@@ -0,0 +1,12 @@
+
+public class QuestGameLevelReduceGhostRoundDefine : QuestGameLevelBaseDefine
+{
+	public override int GameLevelDefine()
+	{
+		var totalReduce = questData.target;
+		//our rule total reduce equal to total passive skill level (id 9 plov kat)
+		var stageUnlockAndUpdateLevel = SMPPassiveSkillLevelConfiguration.GetStageForUnlockNextLevel(0, totalReduce);
+
+		return stageUnlockAndUpdateLevel;
+	}
+}
