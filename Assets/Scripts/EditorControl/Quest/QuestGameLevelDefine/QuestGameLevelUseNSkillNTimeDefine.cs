4c360d125920f4965a1bd61c29cc2a1c8ff089b2
PuthearSMP <bora.chea.sompom@gmail.com>
2022-04-19 14:48:58 +0700

    Apply use time power up

---
+++ Assets/Scripts/EditorControl/Quest/QuestGameLevelDefine/QuestGameLevelUseNSkillNTimeDefine.cs
@@ -0,0 +1,33 @@
+using System;
+
+public class QuestGameLevelUseNSkillNTimeDefine : QuestGameLevelBaseDefine
+{
+	public override int GameLevelDefine()
+	{
+		var nSkill = questData.target;
+		var nTime = questData.target2;
+
+		var manaNeedTotal = 0.0;
+		var skillCostTotal = new SMPNum(0);
+		var maxLevelUnlock = 0;
+		for (int i = 0; i < nSkill; i++)
+		{
+			var skillTargetId = i;
+			var skillData = EditorDatas.instance.GetSkillData(skillTargetId);
+			var skillCost = SMPActiveSkillLevelConfiguration.GetNextCostConfiguration(skillData, 1);
+			skillCostTotal += skillCost;
+			maxLevelUnlock = Math.Max(maxLevelUnlock, skillData.Level_Unlock);
+			
+			var manaConsume = SMPActiveSkillLevelConfiguration.GetNextManaConsumtion(skillData, 1);
+			var manaNeed = manaConsume * nTime;
+			manaNeedTotal += manaNeed;
+		}
+		var manaFarmingPerminute = SMPManaLevelConfiguration.GetManaPerMinutes();
+		var totalTimeMana = manaNeedTotal / (manaFarmingPerminute / 60);
+		var gameLevelOnFarmingMana = GetGameLevelCanReachBaseOnTime(totalTimeMana);
+
+		var gameLevelUnlockSkill = GetGameLevelHeroCanReachLevel(maxLevelUnlock, skillCostTotal);
+
+		return Math.Max(gameLevelUnlockSkill, gameLevelOnFarmingMana);
+	}
+}
