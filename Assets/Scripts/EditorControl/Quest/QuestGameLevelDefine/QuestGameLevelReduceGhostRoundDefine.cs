45ff42d1d924489160389154f1c6b39492365fd6
PuthearSMP <bora.chea.sompom@gmail.com>
2022-04-19 15:23:29 +0700

    Apply pre stage reduce

--- Assets/Scripts/EditorControl/Quest/QuestGameLevelDefine/QuestGameLevelReduceGhostRoundDefine.cs
+++ Assets/Scripts/EditorControl/Quest/QuestGameLevelDefine/QuestGameLevelReduceGhostRoundDefine.cs
@@ -7,6 +7,6 @@ public class QuestGameLevelReduceGhostRoundDefine : QuestGameLevelBaseDefine
 		//our rule total reduce equal to total passive skill level (id 9 plov kat)
 		var stageUnlockAndUpdateLevel = SMPPassiveSkillLevelConfiguration.GetStageForUnlockNextLevel(0, totalReduce);
 
-		return stageUnlockAndUpdateLevel;
+		return stageUnlockAndUpdateLevel - QuestConstance.STAGE_COUNTER_PRE_REACH;
 	}
 }
