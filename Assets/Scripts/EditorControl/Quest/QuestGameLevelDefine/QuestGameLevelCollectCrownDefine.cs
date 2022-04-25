b2e4c02200a73e0df1ed4826a4412408ebf1932f
PuthearSMP <bora.chea.sompom@gmail.com>
2022-04-25 13:59:06 +0700

    Update collect grown

--- Assets/Scripts/EditorControl/Quest/QuestGameLevelDefine/QuestGameLevelCollectCrownDefine.cs
+++ Assets/Scripts/EditorControl/Quest/QuestGameLevelDefine/QuestGameLevelCollectCrownDefine.cs
@@ -3,6 +3,6 @@ public class QuestGameLevelCollectCrownDefine : QuestGameLevelBaseDefine
 {
 	public override int GameLevelDefine()
 	{
-		return questData.target * SMPQuestTemplateConstance.MAX_LEVEL_ON_STAGE - SMPQuestTemplateConstance.STAGE_COUNTER_PRE_REACH;
+		return questData.target * SMPQuestTemplateConstance.MAX_LEVEL_ON_STAGE;
 	}
 }
--- Assets/Scripts/EditorControl/Quest/QuestGameLevelDefine/QuestGameLevelCollectFirstCrownDefine.cs
+++ Assets/Scripts/EditorControl/Quest/QuestGameLevelDefine/QuestGameLevelCollectFirstCrownDefine.cs
@@ -3,6 +3,6 @@ public class QuestGameLevelCollectFirstCrownDefine : QuestGameLevelBaseDefine
 {
 	public override int GameLevelDefine()
 	{
-		return SMPQuestTemplateConstance.MAX_LEVEL_ON_STAGE - SMPQuestTemplateConstance.STAGE_COUNTER_PRE_REACH;
+		return SMPQuestTemplateConstance.MAX_LEVEL_ON_STAGE;
 	}
 }
