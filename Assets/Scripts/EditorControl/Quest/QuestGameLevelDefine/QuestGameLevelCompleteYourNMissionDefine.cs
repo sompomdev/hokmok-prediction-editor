4eed4628ef6d5937c442d45d47b03b342bdb876a
PuthearSMP <bora.chea.sompom@gmail.com>
2022-04-25 16:27:12 +0700

    Update QuestGameLevelCompleteYourNMissionDefine.cs

--- Assets/Scripts/EditorControl/Quest/QuestGameLevelDefine/QuestGameLevelCompleteYourNMissionDefine.cs
+++ Assets/Scripts/EditorControl/Quest/QuestGameLevelDefine/QuestGameLevelCompleteYourNMissionDefine.cs
@@ -4,7 +4,7 @@ public class QuestGameLevelCompleteYourNMissionDefine : QuestGameLevelBaseDefine
 	public override int GameLevelDefine()
 	{
 		var missionCount = questData.target;
-		var gameLevel = (missionCount * 2) - 1;//two level for one mission or one Battle Boss
+		var gameLevel = missionCount;//one level for one mission or one Boss
 		return gameLevel;
 	}
 }
