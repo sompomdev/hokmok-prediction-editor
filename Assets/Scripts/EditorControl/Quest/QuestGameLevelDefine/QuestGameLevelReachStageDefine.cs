ab579596a5410b283f23bde00504f6ba20b0bcad
PuthearSMP <bora.chea.sompom@gmail.com>
2022-04-26 17:54:58 +0700

    Add beat n levels

--- Assets/Scripts/EditorControl/Quest/QuestGameLevelDefine/QuestGameLevelReachStageDefine.cs
+++ Assets/Scripts/EditorControl/Quest/QuestGameLevelDefine/QuestGameLevelReachStageDefine.cs
@@ -3,6 +3,8 @@ public class QuestGameLevelReachStageDefine : QuestGameLevelBaseDefine
 {
 	public override int GameLevelDefine()
 	{
+		//now we can use this for Base Reach Stage, Complete [n]st stage, Beat [n] levels
+		//but for the repeating will change
 		return questData.target;
 	}
 }
