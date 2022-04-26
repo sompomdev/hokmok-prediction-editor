294af36cc3f044341eb3c23ec2df05c3eaf466a7
PuthearSMP <bora.chea.sompom@gmail.com>
2022-04-26 15:29:14 +0700

    Update hero level to reach

--- Assets/Scripts/EditorControl/Quest/QuestGameLevelDefine/QuestGameLevelNHeroReachRankDefine.cs
+++ Assets/Scripts/EditorControl/Quest/QuestGameLevelDefine/QuestGameLevelNHeroReachRankDefine.cs
@@ -4,6 +4,11 @@ public class QuestGameLevelNHeroReachRankDefine : QuestGameLevelBaseDefine
 	public override int GameLevelDefine()
 	{
 		var heroCount = questData.target;
+		if (heroCount == -1)
+		{
+			heroCount = SMPQuestTemplateConstance.MAX_HERO;
+		}
+
 		RankType rankType = (RankType)questData.target2;
 		var heroLvTarget = Helper.GetHeroMinLevelMatchingRankType(rankType);
 		return GetGameLevelFromGoldEarningBallance(heroCount, heroLvTarget);
--- Assets/Scripts/EditorControl/Quest/QuestGameLevelDefine/QuestGameLevelUpgradeNHeroToLvDefine.cs
+++ Assets/Scripts/EditorControl/Quest/QuestGameLevelDefine/QuestGameLevelUpgradeNHeroToLvDefine.cs
@@ -5,6 +5,10 @@ public class QuestGameLevelUpgradeNHeroToLvDefine : QuestGameLevelBaseDefine
 	public override int GameLevelDefine()
 	{
 		var heroTargetCount = questData.target;
+		if (heroTargetCount == -1)
+		{
+			heroTargetCount = SMPQuestTemplateConstance.MAX_HERO;
+		}
 		var heroLevelTarget = questData.target2;
 		var gameLevelCanUpdateHeroLevel = GetGameLevelFromGoldEarningBallance(heroTargetCount, heroLevelTarget);
 		var gameLevelUnlockHero = (heroTargetCount - 1) * SMPQuestTemplateConstance.UNLOCK_HERO_STAGE_COUNT;//-1 is one hero unlock ready
