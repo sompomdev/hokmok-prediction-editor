3bff2a2c3112828d16589d89e6b4492f880511e9
PuthearSMP <bora.chea.sompom@gmail.com>
2022-04-26 17:04:23 +0700

    Add collect diamond from bird

--- Assets/Scripts/EditorControl/Quest/QuestGameLevelDefine/QuestGameLevelCollectNDiamondBirdDefine.cs
+++ Assets/Scripts/EditorControl/Quest/QuestGameLevelDefine/QuestGameLevelCollectNDiamondBirdDefine.cs
@@ -1,13 +1,13 @@
 
-public class QuestGameLevelOpenDiamondChestNTimeDefine : QuestGameLevelBaseDefine
+public class QuestGameLevelCollectNDiamondBirdDefine : QuestGameLevelBaseDefine
 {
 	//collect diamond from bird
 
 	public override int GameLevelDefine()
 	{
-		var diamondChestCount = questData.target;
+		var diamondTarget = questData.target;
 		var timePerDiamondBird = SMPQuestTemplateConstance.TIME_PER_BIRD_APPEAR + SMPQuestTemplateConstance.TIME_PER_BIRD_APPEAR * SMPQuestTemplateConstance.PERCENT_NOT_DIAMOND_BIRD;
-		var timeAvailableAllChest = diamondChestCount * timePerDiamondBird;
+		var timeAvailableAllChest = diamondTarget * timePerDiamondBird;//one chest define as one diamond
 		return GetGameLevelCanReachBaseOnTime(timeAvailableAllChest);
 	}
 }
