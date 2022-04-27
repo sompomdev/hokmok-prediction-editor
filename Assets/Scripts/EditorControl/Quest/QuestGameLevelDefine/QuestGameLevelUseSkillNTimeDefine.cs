b3af6427f2e2fe931c20aca4843a74d157e9eac3
PuthearSMP <bora.chea.sompom@gmail.com>
2022-04-27 16:00:35 +0700

    Update QuestGameLevelUseSkillNTimeDefine.cs

--- Assets/Scripts/EditorControl/Quest/QuestGameLevelDefine/QuestGameLevelUseSkillNTimeDefine.cs
+++ Assets/Scripts/EditorControl/Quest/QuestGameLevelDefine/QuestGameLevelUseSkillNTimeDefine.cs
@@ -8,15 +8,15 @@ public class QuestGameLevelUseSkillNTimeDefine : QuestGameLevelBaseDefine
 		var skillCost = SMPActiveSkillLevelConfiguration.GetNextCostConfiguration(skillData, 1);
 		var gameLevelUnlockSkill = GetGameLevelHeroCanReachLevel(skillData.Level_Unlock, skillCost);
 
-		var useTime = questData.target2;
+		var useTime = questData.target;
 		var manaConsume = SMPActiveSkillLevelConfiguration.GetNextManaConsumtion(skillData, 1);
 		var manaFarmingPerminute = SMPManaLevelConfiguration.GetManaPerMinutes();
 		var manaNeed = manaConsume * useTime;
 		var totalTimeMana = manaNeed / (manaFarmingPerminute / 60);
 		var gameLevelOnFarmingMana = GetGameLevelCanReachBaseOnTime(totalTimeMana);
 
-		//UnityEngine.Debug.Log($"manaConsume {manaConsume.ToString()} manaNeed {manaNeed} manaPerMinute {manaFarmingPerminute} totalTimeMana {totalTimeMana} gameLevelFarm {gameLevelOnFarmingMana}");
+		UnityEngine.Debug.Log($"manaConsume {manaConsume.ToString()} manaNeed {manaNeed} manaPerMinute {manaFarmingPerminute} totalTimeMana {totalTimeMana} gameLevelFarm {gameLevelOnFarmingMana}");
 
-		return gameLevelUnlockSkill;// + gameLevelOnFarmingMana;
+		return gameLevelUnlockSkill + gameLevelOnFarmingMana;
 	}
 }
