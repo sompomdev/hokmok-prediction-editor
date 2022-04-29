8f58760546a35b3f98f8378c2ea9bd8ea3522d9c
PuthearSMP <bora.chea.sompom@gmail.com>
2022-04-29 15:47:43 +0700

    add support n rank

--- Assets/Scripts/EditorControl/Quest/QuestGameLevelDefine/Validated/QuestGameLevelUpgradeSupportNRankDefine.cs
+++ Assets/Scripts/EditorControl/Quest/QuestGameLevelDefine/Validated/QuestGameLevelUpgradeSupportNRankDefine.cs
@@ -11,4 +11,17 @@ public class QuestGameLevelUpgradeSupportNRankDefine : QuestGameLevelBaseDefine
 		var supportId = questData.supportId;
 		return GetGameLevelOnSupportUpgrateLevel(supportId, upgradeLevelCount);
 	}
+	
+	public override int AppearLevelDefine()
+	{
+		/* Formula to get appear gameLevel*/
+		var rankType = (RankType)questData.target;
+		var upgradeLevelCount = Helper.GetSupportMinLevelMatchingRankType(rankType);
+		
+		//Appear before level of support reach to target 100 level
+		upgradeLevelCount -= 100;
+		
+		var supportId = questData.supportId;
+		return GetGameLevelOnSupportUpgrateLevel(supportId, upgradeLevelCount);
+	}
 }
