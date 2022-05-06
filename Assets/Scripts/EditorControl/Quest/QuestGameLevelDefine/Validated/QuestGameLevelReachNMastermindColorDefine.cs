04ca9bb16404b46f2dcf2dc6c429427e1f8a6588
PuthearSMP <bora.chea.sompom@gmail.com>
2022-05-06 11:13:55 +0700

    Add reach n mastermind color in row

--- Assets/Scripts/EditorControl/Quest/QuestGameLevelDefine/Validated/QuestGameLevelReachNMastermindColorDefine.cs
+++ Assets/Scripts/EditorControl/Quest/QuestGameLevelDefine/Validated/QuestGameLevelReachNMastermindColorDefine.cs
@@ -8,6 +8,18 @@ public class QuestGameLevelReachNMastermindColorDefine : QuestGameLevelBaseDefin
 		var reachTarget = questData.target;
 		var gameLevel = GetGameLevelOnFirstPlayMastermind();
 		var gameLevelReachTarget = reachTarget / SMPQuestTemplateConstance.PASS_COLOR_PER_MASTERMINE;
+
+		//don't want appear same as normal play master mind
+		gameLevel += 5;
+		
 		return gameLevel + gameLevelReachTarget;
+	}
+
+	public override int AppearLevelDefine()
+	{
+		var reachTarget = questData.target;
+		var gameLevel = GameLevelDefine();
+		var gameLevelReachTarget = reachTarget / SMPQuestTemplateConstance.PASS_COLOR_PER_MASTERMINE;
+		return gameLevel - gameLevelReachTarget;
 	}
 }
