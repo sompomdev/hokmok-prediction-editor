223b31f0dd98ee5297e3ad28589144b02e663f26
PuthearSMP <bora.chea.sompom@gmail.com>
2022-05-09 11:52:08 +0700

    Add reach thread dps

--- Assets/Scripts/EditorControl/Quest/QuestGameLevelDefine/Validated/QuestGameLevelReachNDPSDefine.cs
+++ Assets/Scripts/EditorControl/Quest/QuestGameLevelDefine/Validated/QuestGameLevelReachNDPSDefine.cs
@@ -6,6 +6,31 @@ public class QuestGameLevelReachNDPSDefine : QuestGameLevelBaseDefine
 	public override int GameLevelDefine()
 	{
 		var dpsTarget = questData.bigTarget_GS;
+		var dps = new SMPNum(0);
+		var tapPerSecond = 5;
+		var heroLv = 1;
+		do
+		{
+			dps = SMPHeroLevelConfiguration.GetDamageWithoutAnyBonus(heroLv) * tapPerSecond;
+			heroLv++;
+		} while (dps < dpsTarget);
+
+		var cost = SMPHeroLevelConfiguration.GetCostOnLevel(5, 1, heroLv - 1);
+		var gameLevel = GetGameLevelCanFarmForCost(cost);
+		return gameLevel;
+	}
+
+	public override int AppearLevelDefine()
+	{
+		var gameLevel = GameLevelDefine();
+		gameLevel -= 5;
+		gameLevel = Math.Max(1, gameLevel);
+		return gameLevel;
+	}
+
+	protected int GetGameLevelByDPSCalculate()
+	{
+		var dpsTarget = questData.bigTarget_GS;
 		var gameLevel = 1;
 		var ghostHp = new SMPNum(0);
 		var dps = new SMPNum(0);
