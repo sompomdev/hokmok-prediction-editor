13ab9b99af9d2734f0a08e7ff4c35293d69637e7
PuthearSMP <bora.chea.sompom@gmail.com>
2022-05-12 16:34:11 +0700

    Add watch video n time

--- Assets/Scripts/EditorControl/Quest/QuestGameLevelDefine/Validated/QuestGameLevelVideoAdsDefine.cs
+++ Assets/Scripts/EditorControl/Quest/QuestGameLevelDefine/Validated/QuestGameLevelVideoAdsDefine.cs
@@ -2,12 +2,18 @@
 public class QuestGameLevelVideoAdsDefine : QuestGameLevelBaseDefine
 {
 	//collect video from bird
+	private int gameLevelStart = 15;
 
 	public override int GameLevelDefine()
 	{
 		var videoCount = questData.target;
 		var timePerAds = SMPQuestTemplateConstance.TIME_PER_BIRD_APPEAR + SMPQuestTemplateConstance.TIME_PER_BIRD_APPEAR * SMPQuestTemplateConstance.PERCENT_NOT_VIDEO_BIRD;
 		var timeAvailableAllChest = videoCount * timePerAds;
-		return GetGameLevelCanReachBaseOnTime(timeAvailableAllChest);
+		return gameLevelStart + GetGameLevelCanReachBaseOnTime(timeAvailableAllChest);
+	}
+
+	public override int AppearLevelDefine()
+	{
+		return gameLevelStart;
 	}
 }
