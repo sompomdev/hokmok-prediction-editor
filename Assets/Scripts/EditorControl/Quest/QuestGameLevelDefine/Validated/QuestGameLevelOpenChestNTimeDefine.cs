1306a425487074c658e9104720890fa857051225
PuthearSMP <bora.chea.sompom@gmail.com>
2022-05-12 17:14:55 +0700

    Open chest n time

--- Assets/Scripts/EditorControl/Quest/QuestGameLevelDefine/Validated/QuestGameLevelOpenChestNTimeDefine.cs
+++ Assets/Scripts/EditorControl/Quest/QuestGameLevelDefine/Validated/QuestGameLevelOpenChestNTimeDefine.cs
@@ -3,10 +3,17 @@ public class QuestGameLevelOpenChestNTimeDefine : QuestGameLevelBaseDefine
 {
 	//or Total of tap on bird
 
+	private int gameLevelStart = 15;
+	
 	public override int GameLevelDefine()
 	{
 		var chestCount = questData.target;
 		var timeAvailableAllChest = chestCount * SMPQuestTemplateConstance.TIME_PER_BIRD_APPEAR;
-		return GetGameLevelCanReachBaseOnTime(timeAvailableAllChest);
+		return gameLevelStart + GetGameLevelCanReachBaseOnTime(timeAvailableAllChest);
+	}
+
+	public override int AppearLevelDefine()
+	{
+		return gameLevelStart;
 	}
 }
